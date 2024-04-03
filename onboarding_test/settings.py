caf235e56eea84437ee510c16971d24f62cd08b7
Michael Ogu <mickeybezalel@gmail.com>
2024-04-03 11:24:07 +0200

    Adding Rest API view

--- onboarding_test/settings.py
+++ onboarding_test/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
     "training.apps.TrainingConfig",
+
 ]
 
 MIDDLEWARE = [
