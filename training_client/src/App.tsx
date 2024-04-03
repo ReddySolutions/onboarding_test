37bf62c73dedf989a7869d20e8521b0f8fa95009
Michael Ogu <mickeybezalel@gmail.com>
2024-04-03 12:07:04 +0200

    Adding responsive grid layout

--- training_client/src/App.tsx
+++ training_client/src/App.tsx
@@ -1,7 +1,24 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button bg="coral">Onborading test UI</Button>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
